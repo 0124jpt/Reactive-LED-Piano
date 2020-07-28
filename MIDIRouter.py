a30a05fa864357ce6fb69ef0e02111bfe56639f4
0124jpt <67206411+0124jpt@users.noreply.github.com>
2020-07-27 22:47:37 -0400

    Update MIDIRouter.py

--- MIDIRouter.py
+++ MIDIRouter.py
@@ -3,17 +3,17 @@ from mididings import *
 config(
     backend='alsa',
     in_ports = [
-        ('piano out', 'Digital Piano:0')
+        ('Controller Out', 'Digital Piano:0') # replace Digital Piano with whatever is the name of the MIDI controller you wish to use
     ],
     out_ports = [
-        ('LED in', 'Arduino Leonardo:0')
+        ('LED In', 'Arduino Leonardo:0') # replace Arduino Leonardo with the name of your MIDI Arduino
     ]
 )
 
 run(
     PortSplit({
         'piano out': [
-            ChannelFilter(1) >> Output('LED in',1),
-        ]
+            ChannelFilter(1) >> Output('LED in', 1), # channel filter is the MIDI channel of the MIDI controller
+        ]                                           # replace '1' with the Arduino MIDI channel you want to route the data to
     })
 )
